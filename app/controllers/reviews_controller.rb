abd9f079f38b6c478b9192f9285114334d4e642f
Robert Lee <bobertsworld@gmail.com>
2023-02-20 23:56:55 -0800

    2nd untracked commit new

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,37 @@
 class ReviewsController < ApplicationController
+    skip_before_action :authenticated_user, only: [:index, :show]
+
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :invalid
+
+
+
+    def index
+        render json: Course.all, status: :ok
+    end
+
+    def show
+        render json: Course.find(params[:id]), status: :ok, serializer: CourseReviewSerializer
+    end
+
+    def create
+        render json: Course.create!(course_params), status: :created
+    end
+
+    def update
+        render json: Course.find(params[:id]).update!(course_params), status: :accepted
+    end
+
+
+
+
+    private
+
+    def course_params
+        params.permit(:name, :location, :price, :holes, :par, :length, :image_url)
+    end
+
+    def not_found
+        render json: { error: "Course not found" }, status: :not_found
+    end
 end
