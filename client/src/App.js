46c0e414571f0a588f33c47520d90fe5d4649162
Robert Lee <bobertsworld@gmail.com>
2023-02-21 00:26:34 -0800

    new commit in new branch with loginlogout

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect} from 'react'
 
 function App() {
+
+  const [currentUser, setCurrentUser] = useState(null)
+
+  useEffect(() => {
+    fetch('/me')
+      .then(res => {
+        if(res.ok) {
+          res.json()
+          .then((user) => setCurrentUser(user))
+        }
+      })
+
+  }, [])
+
+
+  function onLogIn(loggedInUser) {
+    setCurrentUser(loggedInUser)
+
+  }
+
+  function onLogOut(){
+    setCurrentUser(null)
+  }
+  function onShowInfo()
+
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ client/src/components/UserInfo.js
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+
+function UserInfo({ user }) {
+    return(
+        <div className="card-deck col-sm-3 my-3">
+            <Link to={`/users/${user.id}`}>
+                <div class="card text-card">
+                    <img className="card-img-top" alt='me' src={user.image}/>
+                    <h4 className='card-title my-3'>{user.name}</h4>
+                    <div className='card-body'>
+                        <p>{user.number_of_reviews} Reviews</p>
+                        <p>See {user.name}'s information</p>
+                    </div>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default UserInfo
